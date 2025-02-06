cc4053f4ab1ef5186957237bdf94e5c1bb0f4758
kopiPejst92 <agkop01@gmail.com>
2025-02-06 14:31:32 +0100

    adding dependency

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,9 +1,6 @@
-/**
- * Created by jt on 2018-11-26.
- */
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!!!! ");
+        System.out.println("Hello Maven!");
     }
 }
